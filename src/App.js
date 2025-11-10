4ed4df6742c366f43407680c71b93ecf546d7b2b
OscarBayoRequena <oscarbayo2002@gmail.com>
2025-11-10 12:18:42 +0100

    Parameterized comment

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './resources/styles/App.css';
 import Comment from './components/Comment';
 
 function App() {
+
+  let data = {
+    pic: "doge.png",
+    user: "xX_MasterOfTheDark_Xx",
+    background: "Meme page Owner",
+    comment: "Pues la verdad que no se que hago aquí pero ya qe estoy por aquí voy a hacer spam de mi página web de memes. Nunca se pueden tener suficientes memes."
+  }
+
   return (
     <div className="App">
       <h1>Estos son algunos comentarios de nuestros usuarios:</h1>
       <div className='content-container'>
-        <Comment />
+        <Comment data={data} />
       </div>
     </div>
   );
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "../resources/styles/Comment.css"
 
-function Comment (){
+function Comment (props){
 
   return(
     <div className="main-container">
         <div className="user-pic-wrapper">
-          <img className="user-pic" src={require('../resources/images/doge.png')} alt="Sample comment pic" />
+          <img className="user-pic" src={require('../resources/images/'+props.data.pic)} alt="Sample comment pic" />
         </div>
         <div className="text-container">
           <p className="user">
-            xX_MasterOfTheDark_Xx
-             - <span className="background">Meme page Owner</span>
+            {props.data.user}
+             - <span className="background">{props.data.background}</span>
           </p>
-          <p className="main-content">Pues la verdad que no se que hago aquí pero ya qe estoy por aquí voy a hacer spam de mi página web de memes. Nunca se pueden tener suficientes memes.</p>
+          <p className="main-content">{props.data.comment}</p>
         </div>
     </div>
   );
